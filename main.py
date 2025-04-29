67e7801c8e2130559fdeee2bb963f2aa1ebd79cf
1rteem <nasedkintema@gmail.com>
2025-04-29 10:27:10 +0700

    main.py
    Add lose

--- main.py
+++ main.py
@@ -37,10 +37,19 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < 400:
             self.rect.y += self.speed
 
+font.init()
+font1 = font.Font(None,35)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180,0,0))
+lose2 = font1.render('PLAYER 2 LOSE!', True, (180,0,0))
+
+
+
 racket1 = Player('racket.png', 5,10, 50, 100, 3)
 racket2 = Player('racket.png', 640, 10, 50, 100, 3)   
 ball = GameSprite('tenis_ball.png', 250, 250, 50, 50, 3)   
 
+speed_x = 3
+speed_y = 3
 
 finish = False
 run = True
@@ -57,6 +66,25 @@ while run:
         racket2.reset()
         racket1.update_l()
         racket2.update_r()
-        
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.top <= 0:
+            speed_y *= -1
+        if ball.rect.bottom >= 500:
+            speed_y *= -1
+        if ball.rect.colliderect(racket1.rect) or ball.rect.colliderect(racket2.rect):
+            speed_x *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200,200))
+        if ball.rect.x > 650:
+            finish = True
+            window.blit(lose2, (200,200))
+
+
+
+
+
+
     display.update()
     clock.tick(60)
